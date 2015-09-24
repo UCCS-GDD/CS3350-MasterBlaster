d2be461a5a4f715239dbb35e34de90b583bc5116
Ellesent <cool-francesca1@hotmail.com>
2015-09-23 18:16:52 -0600

    Player movement and shooting

---
+++ MasterBlaster/Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MasterBlaster/Assets/Turret.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+    Rigidbody2D turretRigid;
+    public GameObject bullet;
+    float timeDown; 
+
+	// Use this for initialization
+	void Start () {
+        turretRigid = GetComponent<Rigidbody2D>();
+        timeDown = 0;
+	}
+	
+	// Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            turretRigid.AddForce(new Vector2(-0.4f, 0), ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            turretRigid.AddForce(new Vector2(0.4f, 0), ForceMode2D.Impulse);
+        }
+
+    }
+        void Update () {
+            timeDown -= Time.deltaTime; 
+      if (Input.GetKey(KeyCode.Space) && timeDown <= 0)
+      {
+          Instantiate(bullet, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), bullet.transform.rotation);
+          timeDown = 0.5f;
+      }
+
+	}
+}
