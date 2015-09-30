83add082108602d4b4e94b8a6ff6e7340079473a
Ellesent <cool-francesca1@hotmail.com>
2015-09-30 13:10:19 -0600

    Added a shape that falls, stops at the bottom of the screen, and you can destroy the blocks. There is a bug -check trello

---
+++ MasterBlaster/Assets/Sprites/Block.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.y > 0.02)
+        {
+            transform.position -= transform.up * Time.deltaTime * 3;
+
+           //GetComponent<Rigidbody2D>().velocity =  new Vector2(0, -3);
+   
+        }
+       // Debug.Log(pos.y);
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet")
+        {
+            transform.DetachChildren();
+            Destroy(gameObject);
+            Destroy(coll.gameObject);
+        }
+    }
+}
