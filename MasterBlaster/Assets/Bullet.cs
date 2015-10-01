8f90cf3cf3c95f0922f652b6af1c3b67305ec9bb
Ellesent <cool-francesca1@hotmail.com>
2015-09-30 20:34:05 -0600

    fixed movement to stop after awhile and played with bounds of shape -did not work

--- MasterBlaster/Assets/Bullet.cs
+++ MasterBlaster/Assets/Bullet.cs
@@ -5,7 +5,7 @@ public class Bullet : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
 	}
 	
 	// Update is called once per frame
--- MasterBlaster/Assets/Sprites/Block.cs
+++ MasterBlaster/Assets/Sprites/Block.cs
@@ -10,9 +10,25 @@ public class Block : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Bounds combined = GetComponent<Renderer>().bounds;
+        Renderer[] children = GetComponentsInChildren<Renderer>();
+        foreach (Renderer go in children)
+        {
+            combined.Encapsulate(go.bounds);
+            
         
+  
+        }
+
+        GetComponent<Renderer>().bounds.Encapsulate(combined);
+        //GetComponent<Renderer>().bounds.h
+        Debug.Log(combined);
+        
+   
+      
+       
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        if (pos.y > -0.75)
+        if (pos.y > 0.05)
         {
             //transform.position -= transform.up * Time.deltaTime * 3;
 
@@ -24,7 +40,7 @@ public class Block : MonoBehaviour {
             //GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
            GetComponent<Rigidbody2D>().isKinematic = true;
         }
-       Debug.Log(pos.y);
+       
 	}
 
     void OnTriggerEnter2D(Collider2D coll)
