8d19e354da3403fa431f41edd51ef81c6b162241
Ellesent <cool-francesca1@hotmail.com>
2015-10-08 12:37:20 -0600

    can hold s to speed up block falling

--- MasterBlaster/Assets/Sprites/Block.cs
+++ MasterBlaster/Assets/Sprites/Block.cs
@@ -104,6 +104,10 @@ public class Block : MonoBehaviour {
         {
             toMove.GetComponentInParent<Rigidbody2D>().AddForce(new Vector2(0.0005f, 0), ForceMode2D.Impulse);
         }
+        else if (Input.GetKey(KeyCode.S) && toMove != null)
+        {
+            toMove.GetComponentInParent<Rigidbody2D>().AddForce(new Vector2(0, -0.0001f), ForceMode2D.Impulse);
+        }
 
     }
 
@@ -114,42 +118,6 @@ public class Block : MonoBehaviour {
         {
             GetComponentInParent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
             gameObject.tag = "Stationary";
-        }
-    }
-
-    bool isValidGridPos()
-    {
-        Transform[] childs = GetComponentsInChildren<Transform>();
-        foreach (Transform child in childs)
-        {
-            Vector2 v = Grid.roundVec2(child.position);
-
-            // Not inside Border?
-            if (!Grid.insideBorder(v))
-                return false;
-
-            // Block in grid cell (and not part of same group)?
-            if (Grid.grid[(int)v.x, (int)v.y] != null &&
-                Grid.grid[(int)v.x, (int)v.y].parent != transform)
-                return false;
-        }
-        return true;
-    }
-
-    void updateGrid()
-    {
-        // Remove old children from grid
-        for (int y = 0; y < Grid.h; ++y)
-            for (int x = 0; x < Grid.w; ++x)
-                if (Grid.grid[x, y] != null)
-                    if (Grid.grid[x, y].parent == transform)
-                        Grid.grid[x, y] = null;
-
-        // Add new children to grid
-        foreach (Transform child in transform)
-        {
-            Vector2 v = Grid.roundVec2(child.position);
-            Grid.grid[(int)v.x, (int)v.y] = child;
         }
     }
 
