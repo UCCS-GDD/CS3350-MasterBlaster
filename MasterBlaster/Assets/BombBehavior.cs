c17cb73aac4240a69cd3709dc05a1bad97e8cf89
Tyler Dressler <dresslertr@gmail.com>
2015-12-04 04:04:12 -0700

    Bomb Pickup Works FULLY!

---
+++ MasterBlaster/Assets/BombBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBehavior : MonoBehaviour {
+
+    Rigidbody2D RB2D;
+
+	// Use this for initialization
+	void Start () {
+        RB2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D collisionObject)
+    {
+        if (collisionObject.tag == "Block")
+        {
+            Debug.Log("destroying block" + collisionObject.tag);
+            Destroy(collisionObject.transform.root.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+
+	
+	}
+
